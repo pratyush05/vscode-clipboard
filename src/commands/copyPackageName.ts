cd910348195e996fa6ba86fa4bbc2628929a99fd
Pratyush Gupta <pratyush.gupta@druva.com>
2023-03-25 01:36:47 +0530

    Updated package name command

--- src/commands/copyPackageName.ts
+++ src/commands/copyPackageName.ts
@@ -1,8 +1,12 @@
 import * as path from "path";
-import { getFileUri, getGoModulePath, saveToClipboard } from "./utils";
+import { getFileUri, getGoModulePath, getGoPackagePath, saveToClipboard } from "./utils";
 
 export function copyPackageNameCommandHandler(...args: any[]): any {
     const fileUri = getFileUri(args);
-    const packageName = getGoModulePath(fileUri);
+    const goModulePath = getGoModulePath(fileUri);
+    const goPackagePath = getGoPackagePath(fileUri);
+    const packageName = goPackagePath && goPackagePath !== ""
+        ? [goModulePath, goPackagePath].join(path.sep)
+        : goModulePath;
     saveToClipboard(packageName);
 }
--- src/commands/utils.ts
+++ src/commands/utils.ts
@@ -1,13 +1,5 @@
-import * as fs from "fs";
-import * as os from "os";
 import * as path from "path";
 import * as vscode from "vscode";
-
-export function fixDriveCasingInWindows(pathToFix: string): string {
-	return process.platform === "win32" && pathToFix
-		? pathToFix.substring(0, 1).toUpperCase() + pathToFix.substring(1)
-		: pathToFix;
-}
 
 export function getFileUri(args: any[]): vscode.Uri {
     let fileUri = null;
@@ -21,14 +13,31 @@ export function getFileUri(args: any[]): vscode.Uri {
     return fileUri;
 }
 
-export function getGoConfig(fileUri: vscode.Uri): vscode.WorkspaceConfiguration {
-    return vscode.workspace.getConfiguration("go", fileUri);
-}
-
 export function getGoModulePath(fileUri: vscode.Uri): string {
-    const goPath = getGoPath(fileUri);
+    const goPath = getGoPath();
     const workspaceFolderPath = getWorkspaceFolderPath(fileUri);
     const goModulePath = path.relative(goPath, workspaceFolderPath);
+    return sanitizeGoModulePath(goModulePath);
+}
+
+export function getGoPackagePath(fileUri: vscode.Uri): string {
+    const workspaceFolderPath = getWorkspaceFolderPath(fileUri);
+    const folderPath = path.dirname(fileUri.fsPath);
+    const goPackagePath = path.relative(workspaceFolderPath, folderPath);
+    return goPackagePath;
+}
+
+export function getGoPath(): string {
+    const goPath = process.env["GOPATH"];
+	return goPath && goPath !== "" ? goPath : "/go";
+}
+
+export function getWorkspaceFolderPath(fileUri: vscode.Uri): string {
+    const workspace = vscode.workspace.getWorkspaceFolder(fileUri);
+    return workspace ? workspace.uri.fsPath : "";
+}
+
+export function sanitizeGoModulePath(goModulePath: string): string {
     let segments = goModulePath.toLowerCase().split(path.sep);
     const srcIdx = segments.lastIndexOf("src");
     if (srcIdx !== -1) {
@@ -38,99 +47,6 @@ export function getGoModulePath(fileUri: vscode.Uri): string {
     }
 }
 
-export function getGoPath(fileUri: vscode.Uri): string {
-    const workspaceFolderPath = getWorkspaceFolderPath(fileUri);
-    const config = getGoConfig(fileUri);
-    const filePath = fixDriveCasingInWindows(fileUri.fsPath);
-    let inferredGoPath: string | undefined;
-	if (config["inferGopath"] === true) {
-		inferredGoPath = getInferredGoPath(filePath);
-		if (!inferredGoPath) {
-			try {
-				if (fs.statSync(path.join(workspaceFolderPath, "src")).isDirectory()) {
-					inferredGoPath = workspaceFolderPath;
-				}
-			} catch (e) {
-            }
-		}
-		if (inferredGoPath) {
-			try {
-				if (fs.existsSync(path.join(inferredGoPath, "go.mod"))) {
-					inferredGoPath = "";
-				}
-			} catch (e) {
-            }
-		}
-		if (inferredGoPath && process.env["GOPATH"] && inferredGoPath !== process.env["GOPATH"]) {
-			inferredGoPath += path.delimiter + process.env["GOPATH"];
-		}
-	}
-    const configGoPath = config["gopath"] ? resolvePath(substituteEnv(config["gopath"]), workspaceFolderPath) : "";
-	const goPath = (inferredGoPath ? inferredGoPath : configGoPath || process.env["GOPATH"]) ?? "";
-	return goPath;
-}
-
-export function getInferredGoPath(folderPath: string): string | undefined {
-	const segments = folderPath.toLowerCase().split(path.sep);
-	const srcIdx = segments.lastIndexOf("src");
-	if (srcIdx > 0) {
-		return folderPath.substring(0, segments.slice(0, srcIdx).join(path.sep).length);
-	}
-    return;
-}
-
-export function getWorkspaceFolderPath(fileUri: vscode.Uri): string {
-    const workspace = vscode.workspace.getWorkspaceFolder(fileUri);
-    if (workspace) {
-        return fixDriveCasingInWindows(workspace.uri.fsPath);
-    }
-    return "";
-}
-
-export function guessPackageNameFromFile(filePath: string): string {
-    const goFileName = path.basename(filePath);
-    if (goFileName === "main.go") {
-        return "main";
-    }
-    const directoryPath = path.dirname(filePath);
-    const directoryName = path.basename(directoryPath);
-    let segments = directoryName.split(/[.-]/);
-    segments = segments.filter((val) => val !== "go");
-    if (segments.length === 0 || !/[a-zA-Z_]\w*/.test(segments[segments.length - 1])) {
-        return "";
-    }
-    const proposedPackageName = segments[segments.length - 1];
-    fs.stat(path.join(directoryPath, "main.go"), (_, stats) => {
-        if (stats && stats.isFile()) {
-            return "main";
-        }
-        return proposedPackageName;
-    });
-    return proposedPackageName;
-}
-
-export function resolveHomeDir(inputPath: string): string {
-	if (!inputPath || !inputPath.trim()) {
-		return inputPath;
-	}
-	return inputPath.startsWith("~") ? path.join(os.homedir(), inputPath.substr(1)) : inputPath;
-}
-
-export function resolvePath(inputPath: string, workspaceFolder: string): string {
-	if (!inputPath || !inputPath.trim()) {
-		return inputPath;
-	}
-    inputPath = inputPath.replace(/\${workspaceFolder}|\${workspaceRoot}/g, workspaceFolder);
-    inputPath = inputPath.replace(/\${workspaceFolderBasename}/g, path.basename(workspaceFolder));
-	return resolveHomeDir(inputPath);
-}
-
 export function saveToClipboard(text: string): void {
     vscode.env.clipboard.writeText(text);
-}
-
-export function substituteEnv(input: string): string {
-	return input.replace(/\${env:([^}]+)}/g, (_, capture) => {
-		return process.env[capture.trim()] || "";
-	});
 }
